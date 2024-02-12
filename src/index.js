48b08bd161bad4fd58d121a10808579b79e2be42
ZakZanko <zakzanko2112@gmail.com>
2024-02-12 02:35:34 +0200

    delete basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/FIT4-11-practice">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
